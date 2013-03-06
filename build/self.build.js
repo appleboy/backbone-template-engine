ced62b5571f9bfbe76b0edebcd98915b648ae873
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-03-06 15:04:48 +0800

    add require.js self build script.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ build/self.build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: "../assets/js/",
+    name: "main",
+    out: "../assets/js/main-built.js",
+    mainConfigFile: "../assets/js/main.js",
+    preserveLicenseComments: false
+    // support generate Source Maps, make sure requirejs version in 2.1.2
+    // optimize: "uglify2",
+    // generateSourceMaps: true
+})
