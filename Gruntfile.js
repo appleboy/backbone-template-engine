41caa7e96d6f4610c0bc1e28c896e51466818c1e
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-03-16 14:25:32 +0800

    add grunt shell library.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,26 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-shell');
 
     grunt.initConfig({
         exec: {
             build: {
                 command: 'node node_modules/requirejs/bin/r.js -o build/app.build.js'
+            }
+        },
+        shell: {
+            init: {
+                command: 'test -d "assets/vendor" || bower install',
+                options: {
+                    stdout: true,
+                    callback: function (err, stdout, stderr, cb) {
+                        console.log('Install bower package compeletely.');
+                        cb();
+                    }
+                }
+            },
+            template: {
+                command: 'handlebars assets/templates/*.handlebars -m -f assets/templates/template.js -k each -k if -k unless'
             }
         }
     });
@@ -13,5 +29,5 @@ module.exports = function(grunt) {
         grunt.file.mkdir('output');
     });
 
-    grunt.registerTask('default', ['exec', 'copy-output']);
+    grunt.registerTask('default', ['shell', 'copy-output']);
 };
