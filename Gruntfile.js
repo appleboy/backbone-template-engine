5e42ccc5e830f563a1ac2dd9b4c19d2a58ea322d
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-03-16 14:01:45 +0800

    add exec command for grunt js
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,17 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-    exec: {
-        build: {
-            command: 'node node_modules/requirejs/bin/r.js -o build/app.build.js'
-        }
-    }
-    });
-
     grunt.loadNpmTasks('grunt-exec');
+
+    grunt.initConfig({
+        exec: {
+            build: {
+                command: 'node node_modules/requirejs/bin/r.js -o build/app.build.js'
+            }
+        }
+    });
 
     grunt.registerTask('copy-output', function() {
         grunt.file.mkdir('output');
     });
 
-    grunt.registerTask('default', ['copy-output']);
+    grunt.registerTask('default', ['exec', 'copy-output']);
 };
