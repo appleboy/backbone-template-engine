fb3a9b6566a510d7062d267048e11ee1625be247
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-04-14 15:22:10 +0800

    remove js folder.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- app/assets/js/helpers/format_money.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-  var Handlebars, format_money, _selfPrefix;
-  _selfPrefix = module.id;
-  Handlebars = require('handlebars');
-  format_money = require('libs/format_money');
-  return Handlebars.registerHelper('format_money', function(money, ho, dot, delimiter) {
-    return format_money(money, ho, dot, delimiter);
-  });
-});
